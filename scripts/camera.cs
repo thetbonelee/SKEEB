9e3d5c824a520c8b7cc450c71b72701dd75e9c10
thetbonelee <thetbonelee@gmail.com>
2016-09-21 16:09:50 -0600

    Assets folder
    It's a big unorganized mess but we will work on that.

---
+++ scripts/camera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+    public GameObject ball;
+
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(ball == null)
+        {
+            ball = GameObject.Find("Sphere(Clone)");
+        }
+        transform.position = ball.transform.position + offset;
+
+    }
+}
---
+++ scripts/score.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class score : MonoBehaviour {
+
+    public int scoreValue = 0;
+    public TextMesh scoreboard;
+    public TextMesh ballCount;
+    public GameObject ball;
+    public GameObject launcher;
+    public GameObject[] launchers;
+    public static int balls = 8;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        launchers = GameObject.FindGameObjectsWithTag("Launcher");
+        foreach (GameObject launch in launchers)
+        {
+            Destroy(launch);
+        }
+        scoreboard.text = (int.Parse(scoreboard.text) + scoreValue).ToString().PadLeft(4,'0');
+        if (scoreValue != 0)
+        {
+            ballCount.text = "BALLS: " + (--balls).ToString();
+        }
+        if (balls == 0)
+        {
+            if (int.Parse(scoreboard.text) >= 3000)
+            {
+                Application.LoadLevel("win");
+            }
+            else
+            {
+                Application.LoadLevel("lose");
+            }
+        }
+            Instantiate(launcher, new Vector3(0.011f, 0.393f, -10.721f), transform.rotation);
+            Invoke("Reload", 3);
+    }
+}
