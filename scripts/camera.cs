62cb2bd9b5a2afa72815b1b4269d0b66f63fff21
Garrett Glad <garrett.glad@ubnt.com>
2016-09-24 14:15:27 -0600

    Added camera ball follow feature

--- scripts/camera.cs
+++ scripts/camera.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class camera : MonoBehaviour {
@@ -6,18 +6,37 @@ public class camera : MonoBehaviour {
     public GameObject ball;
 
     private Vector3 offset;
+	private float followDist = -3.5f;
+	private float maxZoom = -2.5f;
+	private Vector3 defaultCamera = new Vector3(0, 6, -15);
+	private float delay = 2;
 	// Use this for initialization
 	void Start () {
         offset = transform.position - ball.transform.position;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-        if(ball == null)
-        {
-            ball = GameObject.Find("Sphere(Clone)");
-        }
-        transform.position = ball.transform.position + offset;
 
     }
+
+	void LateUpdate ()
+	{
+
+		ball = GameObject.Find("Sphere(Clone)");
+		if (ball) {
+			if (transform.position.z < maxZoom) {
+				transform.position = ball.transform.position + offset;
+				Vector3 tempPosition = new Vector3 (defaultCamera.x, defaultCamera.y, transform.position.z + followDist);
+				transform.position = tempPosition;
+			}
+		}
+		else if(delay > 0){
+			delay -= Time.deltaTime;
+		}
+		else {
+			transform.position = defaultCamera;
+			delay = 2;
+		}
+	}
 }
