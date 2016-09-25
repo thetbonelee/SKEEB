b0f256b303447bddb5bdc7a2a91e5915b33d9801
Garrett Glad <garrett.glad@ubnt.com>
2016-09-25 09:49:11 -0600

    Set 2 second timeout before camera resets to default

--- scripts/camera.cs
+++ scripts/camera.cs
@@ -10,6 +10,7 @@ public class camera : MonoBehaviour {
 	private float maxZoom = -2.5f;
 	private Vector3 defaultCamera = new Vector3(0, 6, -15);
 	private float delay = 2;
+	private bool zoom = false;
 	// Use this for initialization
 	void Start () {
         offset = transform.position - ball.transform.position;
@@ -29,14 +30,16 @@ public class camera : MonoBehaviour {
 				transform.position = ball.transform.position + offset;
 				Vector3 tempPosition = new Vector3 (defaultCamera.x, defaultCamera.y, transform.position.z + followDist);
 				transform.position = tempPosition;
+				zoom = true;
 			}
 		}
-		else if(delay > 0){
+		else if(delay > 0 && zoom){
 			delay -= Time.deltaTime;
 		}
 		else {
 			transform.position = defaultCamera;
 			delay = 2;
+			zoom = false;
 		}
 	}
 }
