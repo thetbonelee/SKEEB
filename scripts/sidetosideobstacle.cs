0cbbab9014444829d00fe9ae38c7c7351ff4db69
brettmortensen <mortensen.brett@gmail.com>
2016-09-25 15:46:48 -0600

    Added 2 obstacle scripts.
    Added updownobstacle.cs and sidetosideobstacle.cs to the scripts
    folder.

---
+++ scripts/sidetosideobstacle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour
+{
+	private Vector3 left = new Vector3(-5,0,7);
+	private Vector3 right = new Vector3(0,0,7);
+
+	void Update() {
+		transform.position = Vector3.Lerp (left, right, Mathf.PingPong(Time.time * 1.5f, 1.0f));
+	}
+}
---
+++ scripts/updownobstacle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveUp : MonoBehaviour
+{
+	private Vector3 left = new Vector3(0,0.5f,7);
+	private Vector3 right = new Vector3(0,5,7);
+
+	void Update() {
+		transform.position = Vector3.Lerp (left, right, Mathf.PingPong(Time.time * 1.5f, 1.0f));
+	}
+}
